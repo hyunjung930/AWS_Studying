51148bcb42b287f50b175d0c3720b56400851cfc
hyunjung <96286052+hyunjung930@users.noreply.github.com>
2022-04-25 10:16:53 +0900

    Add files via upload
    Test2

---
+++ insertData.py
@@ -0,0 +1,12 @@
+import pymysql.cursors
+
+connection = pymysql.connect(host='sesac-db.cyll0mqkobhi.ap-northeast-2.rds.amazonaws.com',
+                             user='sesac',
+                             password='sesac2022',
+                             database='SesacMovies',
+                             cursorclass=pymysql.cursors.DictCursor)
+with connection:
+    with connection.cursor() as cursor:
+        sql = 'INSERT INTO movies (title, poster) VALUES (%s, %s)'
+        cursor.execute(sql, ('공기살인', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85780/85780_320.jpg'))
+    connection.commit()
