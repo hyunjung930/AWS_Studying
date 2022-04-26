5d54afdf2080cabb8ad8f89245c95245886b23ff
hyunjung <96286052+hyunjung930@users.noreply.github.com>
2022-04-26 22:17:10 +0900

    Add files via upload
    Test2

---
+++ SesacMovie/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import pymysql.cursors
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/movies", methods=['POST'])
+def insert_movie():
+    connection = pymysql.connect(host='sesac-db.cyll0mqkobhi.ap-northeast-2.rds.amazonaws.com',
+                             user='sesac',
+                             password='sesac2022',
+                             database='SesacMovies',
+                             cursorclass=pymysql.cursors.DictCursor)
+    title = request.form['title']
+    poster = request.form['poster']
+    print(f'title: {title}, poster: {poster}')
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = 'INSERT INTO movies (title, poster) VALUES (%s, %s)'
+            cursor.execute(sql, (title, poster))
+        connection.commit()            
+
+    return f'title: {title}, poster: {poster}'
+
+@app.route("/new-movie")
+def show_movie_form():
+    return render_template('upload.html')
+
+
+@app.route("/movies", methods=["GET"])
+def show_movies():
+    connection = pymysql.connect(host='sesac-db.cyll0mqkobhi.ap-northeast-2.rds.amazonaws.com',
+                             user='sesac',
+                             password='sesac2022',
+                             database='SesacMovies',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT id, title, poster FROM movies"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            response = ""
+            for item in result:
+                response += f'<li><img src="{item["poster"]}" height="50"/>{item["title"]}</li>'
+            return f'''
+                    <html>
+                    <body>
+                    <ul>{response}</ul>
+                    </body>
+                    </html>'''
+
+
